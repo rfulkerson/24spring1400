1a50e7c344036bbeb5361bab08529afd0bb534dd
Robert Fulkerson <rfulkerson@unomaha.edu>
2024-02-04 17:13:09 -0600

    Adding Lecture 04

---
+++ Lecture_04/Q5.py
@@ -0,0 +1,4 @@
+val1 = int(input("Enter first: " ))
+val2 = float(input("Enter second: "))
+print(val1, '-', val2, '=', val1 - val2)
+#print(val1)
---
+++ Lecture_04/highlights.py
@@ -0,0 +1,24 @@
+# The highlights file from lecture 04
+
+# x = 12
+# y = 20.2
+# z = "Frank"
+# 
+# age = 12
+# weight_of_food = 20.2
+# first_name = "Frank"
+# 
+# print("Your age is", z)
+# print("Your age is", weight_of_food)
+# 
+# print(x)
+# x = x + 1
+# 
+# tax_rate = float(input("Enter tax rate:"))
+#print(tax_rate*2)
+# 
+# print(f"{tax_rate} is the tax rate")
+# print(f"{tax_rate:.4f} is the tax rate")
+# print(f"{tax_rate:.6f} is the tax rate")
+# print(f"{tax_rate:.2f} is the tax rate")
+# 
